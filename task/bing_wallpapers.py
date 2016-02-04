4b410940876f234238427d559efdc32b63acb345
mark <mark@duitang.com>
2016-02-04 12:35:42 +0800

    something

---
+++ task/bing_wallpapers.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+import json
+import os
+
+image_locate = '/Volumes/tfstore/picture'
+
+bing_wallpapers_url = 'http://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8'
+
+cmd_save_photo = 'wget %s -P %s'
+
+s = requests.session()
+
+result = s.get(bing_wallpapers_url).text
+json = json.loads(result)
+images = json['images']
+for image in images:
+		image_url = image['url']
+		os.system(cmd_save_photo % (image_url,image_locate))
+
+
+
+
